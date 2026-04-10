7dc74aa347b523e5ff7ec0043d68b9b3ed91231a
wahfung <shi_yefeng@163.com>
2026-04-10 12:44:42 +0800

    db01

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,13 @@ export default defineConfig({
   },
   build: {
     assetsInlineLimit: 0,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      strictRequires: true,
+    },
   },
   optimizeDeps: {
-    exclude: ['cesium'],
-    include: ['mersenne-twister', 'urijs', 'autolinker', 'bitmap-sdf', 'dompurify'],
+    // 方案一：一劳永逸，让 Vite 自动处理 cesium 及其所有依赖
+    // 第一次启动稍慢，但后续有缓存就快了，推荐这个方案
   },
 })
